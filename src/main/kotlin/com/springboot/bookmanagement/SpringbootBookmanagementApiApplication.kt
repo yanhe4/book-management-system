e2a5eec50659f8a7ab205b90d79dc4c845810fd6
yanhe4 <heyan_christina@outlook.con>
2022-01-13 14:54:27 +0800

    [Yan He] feat: init git repo

---
+++ src/main/kotlin/com/springboot/bookmanagement/SpringbootBookmanagementApiApplication.kt
@@ -0,0 +1,11 @@
+package com.springboot.bookmanagement
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootBookmanagementApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringbootBookmanagementApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/springboot/bookmanagement/SpringbootBookmanagementApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.springboot.bookmanagement
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootBookmanagementApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
